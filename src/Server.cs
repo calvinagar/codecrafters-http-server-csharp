1e7b5c47b730fc677f932fcd2f0e6896c62480bf
Calvin Agar <calvin.agar@datably.io>
2024-03-19 22:43:58 -0400

    yeehaw2

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,13 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+var socket = server.AcceptSocket(); // wait for client
+
+// read data from connection
+var data = new byte[1024];
+socket.Receive(data);
+
+// return 'HTTP/1.1 200 OK\r\n\r\n'
+var response = "HTTP/1.1 200 OK\r\n\r\n";
+socket.Send(Encoding.ASCII.GetBytes(response));
